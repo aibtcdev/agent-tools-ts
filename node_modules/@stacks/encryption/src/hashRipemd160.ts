39c49736cf8b48fc255b40256789c1e1738a38d5
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-01-30 14:37:13 -0700

    fix: remove node_modules from repo

--- node_modules/@stacks/encryption/src/hashRipemd160.ts
+++
@@ -1,5 +0,0 @@
-import { ripemd160 } from '@noble/hashes/ripemd160';
-
-export function hashRipemd160(data: Uint8Array) {
-  return ripemd160(data);
-}
--- node_modules/c32check/lib/base58check.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function encode(data: string | Uint8Array, prefix?: string | Uint8Array): string;
-export declare function decode(string: string): {
-    prefix: Uint8Array;
-    data: Uint8Array;
-};
