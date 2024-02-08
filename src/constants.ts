7bbe835614be37bbf610e7947de025a2ef2b9d0e
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-02-08 07:34:43 -0700

    fix: update contract deployer and resource name

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const CONTRACT_ADDRESS = "ST17EAYFJ9JDJAQ7RGSE6CTGH90MQH68B3FPR7EKP";
+export const CONTRACT_ADDRESS = "ST2HQ5J6RP8HSQE9KKGWCHW9PT9SVE4TDGBZQ3EKR";
 export const CONTRACT_NAME = "stacks-m2m-v1";
 export const DEFAULT_FEE = 1_000_000; // 1 STX
-export const RESOURCE_NAME = "Bitcoin Face";
+export const RESOURCE_NAME = "bitcoin-face";
 export const RESOURCE_PRICE = 1_000_000; // 1 STX
