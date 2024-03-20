05b6cc45f4fc72ab700fe71da9678bab904db03b
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-03-20 10:36:58 -0700

    feat: create signed msg script, move example to separate file

--- src/wallet/sign-message-example.ts
+++ src/wallet/sign-message-example.ts
@@ -63,10 +63,18 @@ async function main() {
     accountIndex
   );
 
+  console.log(`===== ACCOUNT INFO =====`);
+  console.log(`Network: ${network}`);
+  console.log(`Chain ID: ${networkObj.chainId}`);
+  console.log(`Tx version: ${txVersion}`);
+  console.log(`Account index: ${accountIndex}`);
+  console.log(`Account address: ${address}`);
+
   // SIGNING THE MESSAGE
 
   // create private key obj for signature function
   const privateKey = createStacksPrivateKey(privateKeyString);
+
   // create a domain object as a clarity value
   // based on @stacks.js/transactions signature test
   // https://github.com/hirosystems/stacks.js/blob/fc7e50cb1dca6402677451be06534f0a8f1346b3/packages/transactions/tests/structuredDataSignature.test.ts#L214-L242
@@ -75,8 +83,10 @@ async function main() {
     version: stringAsciiCV("0.0.2"),
     "chain-id": uintCV(networkObj.chainId),
   });
+
   // create the message to be signed as a clarity value
   const message = stringAsciiCV(address);
+
   // sign the message
   // this is type 10: StructuredDataSignature
   const signedMessage = signStructuredData({
@@ -84,8 +94,73 @@ async function main() {
     domain,
     privateKey,
   });
-  // output signed message data
-  console.log(signedMessage.data);
+
+  console.log(`===== SIGNATURE INFO =====`);
+  console.log(`Message: ${cvToValue(message)}`);
+  console.log(`Domain: ${JSON.stringify(cvToJSON(domain), null, 2)}`);
+  console.log(`Signed message type: ${signedMessage.type}`);
+  console.log(`Signed message data: ${signedMessage.data}`);
+
+  // GETTING THE PUBLIC KEY FROM TYPE 10 SIGNATURE (SIP-018)
+
+  // get public key from private key
+  const publicKeyFromPrivate = getPublicKeyFromPrivate(privateKeyString);
+
+  const expectedMessage = encodeStructuredData({
+    message,
+    domain,
+  });
+  const expectedMessageHashed = sha256(expectedMessage);
+
+  const publicKeyFromSignature = publicKeyFromSignatureRsvStructured(
+    bytesToHex(expectedMessageHashed),
+    signedMessage
+  );
+
+  // VALIDATING THE EXPECTED SIGNED MESSAGE
+
+  // test if signature is verified
+  const isTestSignatureVerified = verifyMessageSignatureRsv({
+    signature: signedMessage.data,
+    message: expectedMessageHashed,
+    publicKey: publicKeyFromSignature,
+  });
+
+  console.log(`===== VALIDATION INFO =====`);
+  console.log(`Public key from private:   ${publicKeyFromPrivate}`);
+  console.log(`Public key from signature: ${publicKeyFromSignature}`);
+  console.log(`Signature verified: ${isTestSignatureVerified}`);
+}
+
+function publicKeyFromSignatureVrsStructured(
+  messageHash: string,
+  messageSignature: StructuredDataSignature,
+  pubKeyEncoding = PubKeyEncoding.Compressed
+): string {
+  const parsedSignature = parseRecoverableSignatureVrs(messageSignature.data);
+  const signature = new Signature(
+    hexToBigInt(parsedSignature.r),
+    hexToBigInt(parsedSignature.s)
+  );
+  const point = Point.fromSignature(
+    messageHash,
+    signature,
+    parsedSignature.recoveryId
+  );
+  const compressed = pubKeyEncoding === PubKeyEncoding.Compressed;
+  return point.toHex(compressed);
+}
+
+function publicKeyFromSignatureRsvStructured(
+  messageHash: string,
+  messageSignature: StructuredDataSignature,
+  pubKeyEncoding?: PubKeyEncoding.Compressed
+) {
+  return publicKeyFromSignatureVrsStructured(
+    messageHash,
+    { ...messageSignature, data: signatureRsvToVrs(messageSignature.data) },
+    pubKeyEncoding
+  );
 }
 
 main();
