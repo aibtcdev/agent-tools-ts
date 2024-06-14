339e5d84589fabfa1e3bdafe6b9e2a0668c695e4
Owner <rapha@fontainebleau-management.com>
2024-06-13 22:23:18 -0400

    get-address-by-bnsx

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,10 @@ export const DEPLOYER = "ST2HQ5J6RP8HSQE9KKGWCHW9PT9SVE4TDGBZQ3EKR";
 export const TOKEN_CONTRACT_NAME = "stacks-m2m-aibtc";
 export const TOKEN_NAME = "bridged-bitcoin";
 export const CONTRACT_NAME = "stacks-m2m-v2";
+
+//
+export const BNS_CONTRACT_NAME = "bns";
+export const BNS_CONTRACT_ADDRESS = "SP000000000000000000002Q6VF78";
+
+
+
