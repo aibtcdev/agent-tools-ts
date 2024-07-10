dd8cff86ed7fe1830fd71d80b138cf6f44b8b346
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-07-10 16:00:10 -0700

    fix: add -token to name

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-// used by all scripts calling this contract
-export const DEPLOYER = "ST2HQ5J6RP8HSQE9KKGWCHW9PT9SVE4TDGBZQ3EKR";
-export const TOKEN_CONTRACT_NAME = "stacks-m2m-aibtc";
-export const TOKEN_NAME = "bridged-bitcoin";
-export const CONTRACT_NAME = "stacks-m2m-v2";
