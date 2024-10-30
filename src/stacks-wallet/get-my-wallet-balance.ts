164931ee717e106bbeaa98965e38d0467d6bffc2
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-10-30 14:56:56 -0700

    fix: use acct index, convert from micro

--- src/stacks-wallet/get-my-wallet-balance.ts
+++ src/stacks-wallet/get-my-wallet-balance.ts
@@ -6,6 +6,7 @@ import {
   getNetwork,
   getNetworkByPrincipal,
   deriveChildAccount,
+  microStxToStx,
 } from "../utilities";
 
 async function getAddressBalanceDetailed(address: string) {
@@ -24,15 +25,17 @@ async function getAddressBalanceDetailed(address: string) {
 // Function to format and log balance details
 function logBalanceDetails(balance: any) {
   console.log("STX Balance:");
-  console.log(`  Balance: ${balance.stx.balance}`);
-  console.log(`  Total Sent: ${balance.stx.total_sent}`);
-  console.log(`  Total Received: ${balance.stx.total_received}`);
-  console.log(`  Total Fees Sent: ${balance.stx.total_fees_sent}`);
+  console.log(`  Balance: ${microStxToStx(balance.stx.balance)}`);
+  console.log(`  Total Sent: ${microStxToStx(balance.stx.total_sent)}`);
+  console.log(`  Total Received: ${microStxToStx(balance.stx.total_received)}`);
+  console.log(
+    `  Total Fees Sent: ${microStxToStx(balance.stx.total_fees_sent)}`
+  );
   console.log(
     `  Total Miner Rewards Received: ${balance.stx.total_miner_rewards_received}`
   );
   console.log(`  Lock TX ID: ${balance.stx.lock_tx_id}`);
-  console.log(`  Locked: ${balance.stx.locked}`);
+  console.log(`  Locked: ${microStxToStx(balance.stx.locked)}`);
   console.log(`  Lock Height: ${balance.stx.lock_height}`);
   console.log(`  Burnchain Lock Height: ${balance.stx.burnchain_lock_height}`);
   console.log(
@@ -64,11 +67,24 @@ async function main() {
   // expect txId as first argument
   const network = CONFIG.NETWORK;
   const mnemonic = CONFIG.MNEMONIC;
-  const addr = await deriveChildAccount(network, mnemonic, 0);
+  const accountIndex = CONFIG.ACCOUNT_INDEX;
+
+  // check that values exist for each
+  if (!network) {
+    throw new Error("No network provided in environment variables");
+  }
+  if (!mnemonic) {
+    throw new Error("No mnemonic provided in environment variables");
+  }
+  if (!accountIndex) {
+    throw new Error("No account index provided in environment variables");
+  }
+
+  const { address } = await deriveChildAccount(network, mnemonic, accountIndex);
 
   // get transaction info from API
   try {
-    const balance = await getAddressBalanceDetailed(addr.address);
+    const balance = await getAddressBalanceDetailed(address);
     logBalanceDetails(balance);
   } catch (error) {
     // report error
