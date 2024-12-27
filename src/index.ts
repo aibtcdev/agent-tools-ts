51adb0461471c31c0b15d61b67c5c3b497caa2cb
Ross Ragsdale <rossragsdale@gmail.com>
2024-12-27 06:31:58 -0600

    :rotating_light: Fix bad ref

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 // src/index.ts
 
 // Re-export everything from stacks-dao
-export {
-  DaoSDK,
-  Executor,
-  Treasury,
-  BankAccount,
-  Messaging,
-  Payments,
-} from "./stacks-dao";
+export { DaoSDK, Executor, Treasury, Messaging, Payments } from "./stacks-dao";
 export * from "./stacks-dao/types";
 
 // Namespaced export for compatibility
