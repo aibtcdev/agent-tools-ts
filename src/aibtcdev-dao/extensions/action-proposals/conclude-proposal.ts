59442f576672375380bfad860a80dd0f395ff79a
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-01-13 19:15:15 -0700

    feat: add some action proposal tools
    Creating them will be a bit trickier, will revisit once the contracts are included in the regular deployment.

---
+++ src/aibtcdev-dao/extensions/action-proposals/conclude-proposal.ts
@@ -0,0 +1,87 @@
+import {
+  AnchorMode,
+  broadcastTransaction,
+  getAddressFromPrivateKey,
+  makeContractCall,
+  principalCV,
+  SignedContractCallOptions,
+  uintCV,
+} from "@stacks/transactions";
+import {
+  CONFIG,
+  deriveChildAccount,
+  getNetwork,
+  getNextNonce,
+} from "../../../utilities";
+
+// concludes an action proposal
+async function main() {
+  // contract for the action extension in the dao
+  const [daoActionProposalsExtensionAddress, daoActionProposalsExtensionName] =
+    process.argv[2]?.split(".") || [];
+  // proposal id to be concluded
+  const proposalId = parseInt(process.argv[3]);
+  // action contract in the dao to be executed / concluded
+  const [daoActionProposalContractAddress, daoActionProposalContractName] =
+    process.argv[4]?.split(".") || [];
+  // treasury contract in the dao
+  const [daoTreasuryContractAddress, daoTreasuryContractName] =
+    process.argv[5]?.split(".") || [];
+
+  if (
+    !daoActionProposalsExtensionAddress ||
+    !daoActionProposalsExtensionName ||
+    !proposalId ||
+    !daoActionProposalContractAddress ||
+    !daoActionProposalContractName ||
+    !daoTreasuryContractAddress ||
+    !daoTreasuryContractName
+  ) {
+    console.log(
+      "Usage: bun run conclude-proposal.ts <daoActionProposalsExtensionContract> <daoActionProposalContract> <daoTreasuryContract>"
+    );
+    console.log(
+      "- e.g. bun run conclude-proposal.ts ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.wed-action-proposals ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.wed-action-send-message ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.wed-treasury"
+    );
+
+    process.exit(1);
+  }
+
+  const networkObj = getNetwork(CONFIG.NETWORK);
+  const { key } = await deriveChildAccount(
+    CONFIG.NETWORK,
+    CONFIG.MNEMONIC,
+    CONFIG.ACCOUNT_INDEX
+  );
+  const senderAddress = getAddressFromPrivateKey(key, networkObj.version);
+  const nextPossibleNonce = await getNextNonce(CONFIG.NETWORK, senderAddress);
+
+  const txOptions: SignedContractCallOptions = {
+    anchorMode: AnchorMode.Any,
+    contractAddress: daoActionProposalsExtensionAddress,
+    contractName: daoActionProposalsExtensionName,
+    functionName: "conclude-proposal",
+    functionArgs: [
+      uintCV(proposalId),
+      principalCV(daoActionProposalContractAddress),
+      principalCV(daoTreasuryContractAddress),
+    ],
+    network: networkObj,
+    nonce: nextPossibleNonce,
+    senderKey: key,
+  };
+
+  const transaction = await makeContractCall(txOptions);
+  const broadcastResponse = await broadcastTransaction(transaction, networkObj);
+
+  console.log(`Proposal concluded successfully: 0x${broadcastResponse.txid}`);
+  console.log(`Full response: ${JSON.stringify(broadcastResponse, null, 2)}`);
+}
+
+main().catch((error) => {
+  error instanceof Error
+    ? console.error(JSON.stringify({ success: false, message: error.message }))
+    : console.error(JSON.stringify({ success: false, message: String(error) }));
+
+  process.exit(1);
+});
