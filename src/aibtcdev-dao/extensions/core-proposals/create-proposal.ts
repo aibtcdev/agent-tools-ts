959d481c5f68038f970793907ee6e4b0086c9171
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-01-13 18:40:59 -0700

    fix: update usage info with new name

--- src/aibtcdev-dao/extensions/core-proposals/create-proposal.ts
+++ src/aibtcdev-dao/extensions/core-proposals/create-proposal.ts
@@ -27,10 +27,10 @@ async function main() {
     !proposalContractName
   ) {
     console.log(
-      "Usage: bun run create-new-core-proposal.ts <daoCoreProposalExtensionContract> <newProposalContract>"
+      "Usage: bun run create-proposal.ts <daoCoreProposalExtensionContract> <newProposalContract>"
     );
     console.log(
-      "- e.g. bun run create-new-core-proposal.ts ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.wed-core-proposals ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.aibtc-base-bootstrap-initialization"
+      "- e.g. bun run create-proposal.ts ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.wed-core-proposals ST35K818S3K2GSNEBC3M35GA3W8Q7X72KF4RVM3QA.aibtc-base-bootstrap-initialization"
     );
 
     process.exit(1);
