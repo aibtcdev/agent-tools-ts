88cdd00b259556afcbe1646a44dd612905038666
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-02-26 14:46:28 -0700

    feat: build contract deployer around dao deployer logic

---
+++ src/stacks-contracts/services/contract-deployer.ts
@@ -0,0 +1,74 @@
+import { StacksNetworkName } from "@stacks/network";
+import {
+  DeployedSingleContract,
+  getNetworkTypeFromName,
+  NetworkName,
+  SingleContract,
+} from "../types/dao-types-v2";
+import {
+  AnchorMode,
+  broadcastTransaction,
+  makeContractDeploy,
+  PostConditionMode,
+} from "@stacks/transactions";
+
+/**
+ * Contract deployment service for single contracts
+ */
+export class ContractDeployer {
+  private network: StacksNetworkName;
+  private senderAddress: string;
+  private senderKey: string;
+
+  /**
+   * Create a new ContractDeployer instance
+   *
+   * @param network Network instance to use for deployments
+   * @param senderAddress Address that will deploy the contracts
+   * @param senderKey Private key for the sender address
+   */
+  constructor(network: NetworkName, senderAddress: string, senderKey: string) {
+    this.network = getNetworkTypeFromName(network);
+    this.senderAddress = senderAddress;
+    this.senderKey = senderKey;
+  }
+
+  /**
+   * Deploy a contract to the blockchain
+   *
+   * @param contract The generated contract to deploy
+   * @param nonce Optional nonce for the transaction
+   */
+  async deployContract(
+    contract: SingleContract,
+    nonce?: number
+  ): Promise<DeployedSingleContract> {
+    // Create the contract deployment transaction
+    const transaction = await makeContractDeploy({
+      contractName: contract.name,
+      codeBody: contract.source,
+      senderKey: this.senderKey,
+      nonce: nonce ? nonce : undefined,
+      network: this.network,
+      anchorMode: AnchorMode.Any,
+      postConditionMode: PostConditionMode.Allow,
+    });
+
+    // Broadcast the transaction
+    const broadcastResponse = await broadcastTransaction(
+      transaction,
+      this.network
+    );
+
+    if (broadcastResponse.error) {
+      throw new Error(`Failed to deploy contract: ${broadcastResponse.reason}`);
+    }
+    // Return the deployed contract details
+    return {
+      ...contract,
+      txId: broadcastResponse.txid,
+      contractAddress: `${this.senderAddress}.${contract.name}`,
+      sender: this.senderAddress,
+    };
+  }
+}
