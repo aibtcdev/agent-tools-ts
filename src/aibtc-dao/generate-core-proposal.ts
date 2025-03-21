0a4d96589522d3e6610916dc8cb92eedf9ceba74
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-03-20 23:21:57 -0700

    feat: generate core proposal starter file

---
+++ src/aibtc-dao/generate-core-proposal.ts
@@ -0,0 +1,3 @@
+const usage = "Usage: bun run generate-core-proposal.ts <coreProposalName>";
+const usageExample =
+  "Example: bun run generate-core-proposal.ts aibtc-core-proposals-set-proposal-bond";
