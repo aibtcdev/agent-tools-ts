cf6c773962dd6e75a657e3aad3f7b19a68333f8b
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-06-30 15:05:30 -0700

    feat: Add print-mission utility with DAO mission and inscription details

---
+++ src/aibtc-cohort-0/utils/print-mission.ts
@@ -0,0 +1,157 @@
+const daoMission = `
+## MISSION
+
+To make Bitcoin Faces the most popular meme amongst Bitcoiners, on X, onchain, and throughout Bitcoin culture.
+
+## GUIDELINES
+
+In pursuit of our mission, the DAO adheres to these guidelines:
+1. Face as Identity: Each Bitcoin Face is a unique, generative avatar tied to a name. It reflects both sovereignty and style.
+2. Meme Distribution: The DAO funds and coordinates the viral spread of Bitcoin Faces across X, onchain platforms, and beyond. Contributors are rewarded for anything that helps, including memes, art, stories, tools, apps, and more.
+3. CC0 Public Domain Dedication: All Bitcoin Faces content is permanently released into the public domain. Anyone can use, remix, adapt, or commercialize without permission or restriction.
+4. Community Curation: While anyone can generate or remix a face, the DAO curates which styles, transformations, and extensions guide the official direction of Bitcoin Faces culture.
+5. Minting Access: Bitcoin Faces can be generated and minted at bitcoinfaces.xyz or through any app that references the canonical onchain inscriptions.
+6. Canonical Inscriptions: The Bitcoin Faces collection is based on 95 original onchain attributes and a public generator. These inscriptions, listed below, define the visual foundation of the meme.
+
+## INSCRIPTIONS
+
+### Generator (2)
+
+- [#28334467](https://ordinals.com/inscription/738ec4b6833ca6392cfc2f2255da1c8e9436abc911f909d047ff65cede2a63dai0)
+- [#28054160](https://ordinals.com/inscription/2a6c95c16d15933a703ee2466fc4c7e64967f0c94768553b3fd9d6986e528486i0)
+
+### Heads Attributes (13)
+
+- [#27475806](https://ordinals.com/inscription/a34c2814fdbf1667f7dc10a891b3613d53595863b1f286afdbd94084c8964608i0)
+- [#27475874](https://ordinals.com/inscription/1bb84b7b32be7acd53ff29c21f8c3254fc557cb9dcc7df9a72bc9e4ed021ab19i0)
+- [#27475877](https://ordinals.com/inscription/b88b81635faad19f654f28fd5502c4a5008a073cb24529568d7bc9e9f6584420i0)
+- [#27475842](https://ordinals.com/inscription/14fead64cb0dfbd58addb5537a567aa643606d3a7989e92431e37072421f552ai0)
+- [#27475879](https://ordinals.com/inscription/8f27e473312d9442441fd2cc30b328400517648cce605701411f6ad00b114e58i0)
+- [#27475878](https://ordinals.com/inscription/7b8de69d565b4d69b4232fc76ed029acb82bce05f0dd65a72da3027cd6f374aai0)
+- [#27475881](https://ordinals.com/inscription/703b6358a3ae55f190a653cd51bcca5251b85cb99a2c5d5b130f1285df33c0afi0)
+- [#27475853](https://ordinals.com/inscription/fa4e61a854c673a223dda13ec8b934db01069feff6652134824e2d2c6913febci0)
+- [#27475833](https://ordinals.com/inscription/f0b78ca9093ca8eccab4a521a1691d640a20ad8caa8083d1f329420c4964b0c1i0)
+- [#27475848](https://ordinals.com/inscription/c029f0afeea66091a573fba60dbe3e5e7301828dc520610a9eae120364a6b5d0i0)
+- [#27475870](https://ordinals.com/inscription/57dec6b488d4a9c216193fa286b9eb4a70efe550fc08a4a4e791c27736088ad4i0)
+- [#27475829](https://ordinals.com/inscription/f08ab33010681eaacee2200d6403de61cc68e7fa3ab97afc33452e47879512ddi0)
+- [#27475844](https://ordinals.com/inscription/bd098d6bc951530e66fdd0847ff1cc4ce0a61cf19b930e584f87d4e7f7820bfai0)
+
+### Hat/Horn Attributes (7)
+
+- [#27475811](https://ordinals.com/inscription/5e47b9e95cadf202b5c51f0e40851c63b74b57eaf4392102a0198927bd7b9325i0)
+- [#27475866](https://ordinals.com/inscription/853db99e14d11f5abd62bd621b488bc507a887bf441bef1a1c773c2f78b70d2fi0)
+- [#27475852](https://ordinals.com/inscription/807f95d11e688a31a7769ed902bde7f71bea2983e17e32b5e98a2f2d1e186637i0)
+- [#27475841](https://ordinals.com/inscription/f582a86538f754f1d41f3a91106eaf7c1b2b645988cd786a5b6dae4442053b5ei0)
+- [#27475849](https://ordinals.com/inscription/d65bbbaf2f1e193492c801c33066b19953a36248acf50d3b52e581a13ee43c88i0)
+- [#27475856](https://ordinals.com/inscription/f20b28f2e7bbb7af2dc727c860161d9c56725b7b5d89c3ccc50790f87fdbf38ci0)
+- [#27475854](https://ordinals.com/inscription/d2b74f9ba8e9af35ae7b1ac8e2f9a91aaa46a1cad971ed2255ec3e8f55fe25eci0)
+
+### Body Attributes (6)
+
+- [#27475846](https://ordinals.com/inscription/4891fc56d297684275f904cbd5747537d02f3c8fefe5731d3f2797cc28589b31i0)
+- [#27475851](https://ordinals.com/inscription/7bde2d6715aaa157ee7f92259d8c966c63d7033e25e5d4a6aef806337412c616i0)
+- [#27475864](https://ordinals.com/inscription/d31c63df52207f90f969688b147c31492fc697bec3b2f7367e02c16e0004a0d4i0)
+- [#27475880](https://ordinals.com/inscription/0f478d4ea74e1fb8ddfd73b36d9f89a6c8f9fdd05192b0d817f372c2deecc4d5i0)
+- [#27475871](https://ordinals.com/inscription/e12c3c1efb548de352387fed30413fc07ae27137ce9d81b9e0b06bd1f67655e2i0)
+- [#27475831](https://ordinals.com/inscription/0991d1034aa2f40b13b82774f9b6faccee23b037ad352b5edcca27e8cb05bde9i0)
+
+### Eye Attributes (9)
+
+- [#27475838](https://ordinals.com/inscription/2630332bbc0bc3bc1b17b4be225ba41ae58dae5cb74cbf33f150182363f13d02i0)
+- [#27475855](https://ordinals.com/inscription/a730de0bf8343fbf21abc2376d83359ca1a6afe23614a00b2046902a8b11df02i0)
+- [#27475805](https://ordinals.com/inscription/616a8c9ee80d36e5bc2bf214c2b0729176d9f2dc0187ad8d72cbd5ff2311d204i0)
+- [#27475839](https://ordinals.com/inscription/4401e04f9748fcfa0e07347f88b1d3ac3fa8ee3f5401cc1cc7dc3d49471ac40ei0)
+- [#27475835](https://ordinals.com/inscription/9dd4f5aa1287ddb695eb4b2381f58dd7a6e8706ed1e7f376ed18535b81923e1bi0)
+- [#27475868](https://ordinals.com/inscription/629079513bdae4bc955fbb309fdbc8092ed4761f43e2c46144cc8cb04644921bi0)
+- [#27475812](https://ordinals.com/inscription/cca92c88e93d5bbcc98c63c26d9e2b02df76a23f8d85e91ef3a888d62f86ab27i0)
+- [#27475830](https://ordinals.com/inscription/2cde18753b4ea17923dffce90e369039351070b433fd1b15eab6ef31587d3fe0i0)
+- [#27475861](https://ordinals.com/inscription/1af1acdf4834c0c8aa73c4c6dc44c30df3213bf95d7c08c9bc51fb1bd1084febi0)
+
+### Laser Eye Attributes (23)
+
+- [#32677943](https://ordinals.com/inscription/c897eb21979fe6a9e5c7b8cb04713478082dce92b8c6f2a348c23f16542aea07i0)
+- [#32677938](https://ordinals.com/inscription/612f727425302b50b30e84fb54fdcd17ebc58649b3583c39c5b12828ab907312i0)
+- [#32677987](https://ordinals.com/inscription/d4c469f2902ede17d1907c167db721a3f0e3cfef7a8305333a5fcb6db343aa12i0)
+- [#32677954](https://ordinals.com/inscription/81d57836b6a3485d5d39c7c083f1ed829ddd92aabd309c9b752e6c87e37a0531i0)
+- [#32677940](https://ordinals.com/inscription/4e98536baf44cd5aa85f994935f9b25443bb51059f54b7ed6cc43d009e123834i0)
+- [#32677988](https://ordinals.com/inscription/c68476a8d53ce950475ad4044df16087f0dddaf2af244e190b6b288ec88c7038i0)
+- [#32677993](https://ordinals.com/inscription/544056931898458410473610eaef98c5cf6b1fe2c696f22ef65aed77a4445a39i0)
+- [#32677962](https://ordinals.com/inscription/192e54e66ac1559f6f596e68a491fb423a95e38bc275029f9afb642658eb7e4ci0)
+- [#32677963](https://ordinals.com/inscription/d99bd2b865dadf20b59c231a4afc738e995752027592aa53e125aa8825b5884di0)
+- [#32677990](https://ordinals.com/inscription/c099d0562e0aee188fffb864dba2f5ef066de8a5524e8d951c30dc9a48525357i0)
+- [#32677966](https://ordinals.com/inscription/9e7cd2891721515bfc8cd59240090e385078630e3ce1c3a907011cffd374855di0)
+- [#32677992](https://ordinals.com/inscription/eff313023bdf091a672e72c94b4c200b47514871520c3cdd1fb90d151362dd65i0)
+- [#32677991](https://ordinals.com/inscription/0128d17f0c0dd2cc14acbb029ee15d22757b38ab30cf4ff4ce9afb837da49a85i0)
+- [#32677939](https://ordinals.com/inscription/98e99ca432e19dea6ba035d68501204549c4051911182ac689baba65727b2c89i0)
+- [#32677934](https://ordinals.com/inscription/554d0583f70b024c754bc22f529822c6f69b7a4dee6d7894fff0d10bf4069592i0)
+- [#32677989](https://ordinals.com/inscription/4bd9a298dd80421f63753d25a3316753f9bd38a4a1eb57e2b1175ff20ea8ee93i0)
+- [#32677936](https://ordinals.com/inscription/8122b055864f80e3362803cb8feb170dc879969b5819a7f5d8abe7a39718faa9i0)
+- [#32677994](https://ordinals.com/inscription/b3f04261d6035985227421d00eb4c9ab83bb2a246d5d3d785089d22a1fe258bai0)
+- [#32677986](https://ordinals.com/inscription/71551f31f2dd84dddf4f977f45dda483f42fc03ead17c4e79a4ea0ac4df4c5bai0)
+- [#32677935](https://ordinals.com/inscription/beb52dea3c1696b27cf17cbc87896b03f42b77c1d659e336619b6cf2eed9fdd1i0)
+- [#32677980](https://ordinals.com/inscription/d0caf0b8fc41b8fa2adaef05eff6f04d58ca2c1dab967be9bcc449c087ef3cd2i0)
+- [#32677937](https://ordinals.com/inscription/a2013a8b7bce76e108e60a0338efeaf35d1eb4f1499e2dae071eff9266d3c2d2i0)
+- [#32677995](https://ordinals.com/inscription/459b18ab7d5cf0feec4f05a07ed54ba6c3e9c0a2e9c081b85557fbc7d404dfe1i0)
+
+### Mouth Attributes (8)
+
+- [#27475850](https://ordinals.com/inscription/f46333c7aae731f501743274b0b73f59c1863233a2948a4d42ca580b4a315600i0)
+- [#27475823](https://ordinals.com/inscription/4e7f5ec1f5b9d7329436a4221daed1bdd75becefd4f4c082b05daaf0e9ff6566i0)
+- [#27475837](https://ordinals.com/inscription/fe59c820ccaf747c17e3fbbd88d5050b347de35481dfac1522506116f7175868i0)
+- [#27475825](https://ordinals.com/inscription/b1be0eefa03bb914c7d7f32ae613b4a4f0be3b1c6d9439e211488269d85b3e86i0)
+- [#27475862](https://ordinals.com/inscription/2508ea3801a435aade9f998c8e600b17d6f1695a3e9f2f892175785c73e2ab8di0)
+- [#27475840](https://ordinals.com/inscription/fbb61aeca3fe079da78cf701b56150c63e83014eb07f5ad834692aca5f0ca3aei0)
+- [#27475873](https://ordinals.com/inscription/f3216c3ef151c8c2814f1ab675ff7cb3f85b612beffb98d8e5a1a7ab4b51d5bei0)
+- [#27475875](https://ordinals.com/inscription/59efa6f17a8c331e9439ddce026fa058754f95f3a1e02d534f6e7f2ef89c15afi0)
+
+### Nose Attributes (7)
+
+- [#27475860](https://ordinals.com/inscription/8845fd90d6d13c76b59c4c769104160eac451f478cfd4efdf3b40ca8fb024c7ai0)
+- [#27475872](https://ordinals.com/inscription/87191d4187f614618f37629169f5b552bdaa9a2aac4b226e693885023a181d7di0)
+- [#27475857](https://ordinals.com/inscription/b439feeb2356b77bdc63617dc8a3437b0cbbe4d261b8f2041eb4d81bfec92da7i0)
+- [#27475869](https://ordinals.com/inscription/25fda933bd0e592c486bfa880e50970178a048c6df5c50b1741a2f1d6585cad7i0)
+- [#27475859](https://ordinals.com/inscription/7b16e48fbc6658a6646b660d50af0f955abaa37bb1b8f0532ba53158bf71a6dbi0)
+- [#27475865](https://ordinals.com/inscription/3e98ea13246556fdf97bbff57b88a764d3e3a5c27de900877fb42f4c6b23c6f8i0)
+- [#27475876](https://ordinals.com/inscription/86e63c18bd14c72f6ff877e587818e626df2c80627e36b3db9f70fac507f842ei0)
+
+### Ear Attributes (5)
+
+- [#27475814](https://ordinals.com/inscription/cf392227692106ca35c1d02b0f65dafbef129e3c42f568ecf4845dcd386aae30i0)
+- [#27475817](https://ordinals.com/inscription/917d6ef4102d95122adcceb1482b78545d4bb13ff835c109c2016a5ff919483ei0)
+- [#27475836](https://ordinals.com/inscription/94dcdf1067cde2ac108d41d82bf6ab06fe08f7985572a57bf9c24a9d13baa77bi0)
+- [#27475847](https://ordinals.com/inscription/49f8014d388de06928f604d2ac6eb7380d2fcfa441570ce96004e62da21945b5i0)
+- [#27475867](https://ordinals.com/inscription/cbc7d3b7a0f01aee2bb7474bb74bc87ff3969e65f3761b8d92412f2942ca95d7i0)
+
+### Glasses Attributes (3)
+
+- [#27475834](https://ordinals.com/inscription/c31ebd0d68e065e640f37365ea912fd8d40ad260d964d2f02c8db7016913cc8ai0)
+- [#27475863](https://ordinals.com/inscription/1dc0482c2595e4e4dc5aedc73165c9efce0a12735933e9bb171de91b8ca5d0b8i0)
+- [#27475845](https://ordinals.com/inscription/51318d541ccc19345873152f5804171942e9a088bb19f2ff6ea7ac8977dbc7ebi0)
+
+### Earring Attribute (1)
+
+- [#27475827](https://ordinals.com/inscription/62b558e9d4000390aab0e06dde66b6671c6e473d033724f9db31c46536642f9fi0)
+
+### Necklace Attribute (1)
+
+- [#27475820](https://ordinals.com/inscription/6ec67f9a3061fc866c3cbcd9c1a30b2ae1c9e3c20f486bd689d3e3886bbf725di0)
+
+### Background Attributes (10)
+
+- [#27475807](https://ordinals.com/inscription/c8f8e2e179fcbec4624d52b9118349bc01414a839e01e399a6ccfa42ce1c150ai0)
+- [#27475808](https://ordinals.com/inscription/2c82987ab59df2077bd9b2522f823155cc3ca306a14094c1ce3ca94ad13bb915i0)
+- [#27475815](https://ordinals.com/inscription/3b3d044b82de333e8310c2df1d55e02936ae1e5d16966ce86e9269904b50da31i0)
+- [#27475816](https://ordinals.com/inscription/d93d0b543ed765906e882da47cac7969efcb9930abe5495e52b1c777675aac32i0)
+- [#27475818](https://ordinals.com/inscription/b1d6a465edc6bf876e1f67adb6951e7041168ca2686cc0e970901a7fce59b044i0)
+- [#27475821](https://ordinals.com/inscription/327e7279dc7a69d2a28bafcc7d215511a8292f5b9514ab57756d57d8cb385463i0)
+- [#27475822](https://ordinals.com/inscription/8723751cfc76f030dcd8c7be883fc0ab95eec8a4274f55caf00d20f151fbfe64i0)
+- [#27475824](https://ordinals.com/inscription/6c6b83ccf5db73fd371f0dcaa62fb607c58dc86c99b068982f9c28e112328781i0)
+- [#33238535](https://ordinals.com/inscription/b0542489cd776405d366ad02fdf40879b450448fe2945e0beb81f65ba97581bdi0)
+- [#27475828](https://ordinals.com/inscription/e4e62d2d1f7d0c8a16b8ed5f832f08ed12c60903723d2c091189156c2289f4cai0)
+`;
+
+export function printMission() {
+  console.log(JSON.stringify(daoMission));
+}
+
+printMission();
