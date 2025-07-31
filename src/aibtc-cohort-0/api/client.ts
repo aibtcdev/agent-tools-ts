30e9e0e909052dad3ed82d540e9555d3a450fc8a
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-07-30 18:27:27 -0700

    fix: update client after verifying feature

--- src/aibtc-cohort-0/api/client.ts
+++ src/aibtc-cohort-0/api/client.ts
@@ -13,14 +13,13 @@ export class ContractApiClient {
   private baseUrl: string;
 
   constructor() {
-    /* select endpoint based on CONFIG
+    // select endpoint based on CONFIG
     this.baseUrl =
       CONFIG.NETWORK === "mainnet"
         ? "https://daos.aibtc.dev/api"
         : "https://daos-staging.aibtc.dev/api";
-        */
     // temporary override for faster development cycles
-    this.baseUrl = "https://aibtcdev-daos-preview.hosting-962.workers.dev/api";
+    // this.baseUrl = "https://aibtcdev-daos-preview.hosting-962.workers.dev/api";
   }
 
   async getAllContracts() {
